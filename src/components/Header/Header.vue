03731c894c2c76dc0d7a40a929ca71794f6e4adc
Fahim Hossain Munna <87969487+Fahim-Hossain-Munna@users.noreply.github.com>
2024-01-17 15:39:18 +0600

    header part complete

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -3,12 +3,19 @@
 </script>
 
 <template>
-    <div class="bg-black w-full py-4">
-        <div class="max-w-[1170px] mx-auto flex justify-end">
-            <p class="text-white font-pop text-sm">Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
-                <a href="#">ShopNow</a>
+    <header class="bg-black w-full py-4">
+        <div class="max-w-[1170px] mx-auto flex items-center justify-between">
+            <p class="w-full text-center text-white font-pop text-sm">Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+                <a href="#" class="font-pop text-sm font-bold ml-1 underline">ShopNow</a>
             </p>
+            <select class="font-pop text-sm outline-none bg-transparent text-white pr-[5px]">
+                <option value="English" class="bg-black">English</option>
+                <option value="English" class="bg-black">English</option>
+                <option value="English" class="bg-black">English</option>
+                <option value="English" class="bg-black">English</option>
+            </select>
         </div>
-    </div>
+    </header>
+
 </template>
 
