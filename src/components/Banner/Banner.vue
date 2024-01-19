6eac8f0c862836a5016f5260d9d27ca8b167fe45
Fahim Hossain Munna <87969487+Fahim-Hossain-Munna@users.noreply.github.com>
2024-01-19 19:40:58 +0600

    banner part responsive for mobile and pc

--- src/components/Banner/Banner.vue
+++ src/components/Banner/Banner.vue
@@ -3,15 +3,17 @@ import Icon from '../layouts/Icon.vue';
 import CategoryList from '../layouts/CategoryList.vue';
 import Picture from '../layouts/Picture.vue';
 import Container from '../layouts/Container.vue';
+import { ref } from 'vue';
 
+let showUl = ref(false);
 
 </script>
 
 <template>
     <div>
         <!-- <div class="max-w-[1170px] mx-auto flex"> -->
-            <Container className="flex">
-            <div class="w-[233px] border-r pt-10">
+            <Container className="lg:flex">
+            <div class="w-[233px] hidden lg:block border-r pt-10">
                 <ul>
                     <CategoryList CategoryTitle="Woman’s Fashion" position="relative pb-4 last:pb-0" classCode="absolute top-0 right-4"/>
                     <CategoryList CategoryTitle="Men’s Fashion" position="relative pb-4 last:pb-0" classCode="absolute top-0 right-4"/>
@@ -24,9 +26,29 @@ import Container from '../layouts/Container.vue';
                     <CategoryList CategoryTitle="Health & Beauty" position="relative pb-4 last:pb-0" classCode="absolute top-0 right-4 hidden"/>
                 </ul>
             </div>
-            <div class="w-[937px] pt-10 pl-11">
-                <div class="h-[344px]">
-                    <Picture source="/src/assets/uploads/banner/banner.jpg" className="w-full h-full" alt="/src/assets/uploads/banner/banner.jpg" navigate="/home" />
+            <div class="w-full pl-2 my-4 lg:mt-0 lg:hidden">
+                <a class=" text-black text-base relative" @click="showUl =! showUl">Show Category 
+                <Icon iconName="keyboard_double_arrow_right" class="absolute top-0"/>
+                </a>
+                <Transition>
+                <div v-if="showUl" class="transition-all duration-200">
+                    <ul class="my-6">
+                    <CategoryList CategoryTitle="Woman’s Fashion" position="relative pb-4 last:pb-0" classCode="absolute top-0 right-4"/>
+                    <CategoryList CategoryTitle="Men’s Fashion" position="relative pb-4 last:pb-0" classCode="absolute top-0 right-4"/>
+                    <CategoryList CategoryTitle="Electronics" position="relative pb-4 last:pb-0" classCode="absolute top-0 right-4 hidden"/>
+                    <CategoryList CategoryTitle="Home & Lifestyle" position="relative pb-4 last:pb-0" classCode="absolute top-0 right-4 hidden"/>
+                    <CategoryList CategoryTitle="Medicine" position="relative pb-4 last:pb-0" classCode="absolute top-0 right-4 hidden"/>
+                    <CategoryList CategoryTitle="Sports & Outdoor" position="relative pb-4 last:pb-0" classCode="absolute top-0 right-4 hidden"/>
+                    <CategoryList CategoryTitle="Baby’s & Toys" position="relative pb-4 last:pb-0" classCode="absolute top-0 right-4 hidden"/>
+                    <CategoryList CategoryTitle="Groceries & Pets" position="relative pb-4 last:pb-0" classCode="absolute top-0 right-4 hidden"/>
+                    <CategoryList CategoryTitle="Health & Beauty" position="relative pb-4 last:pb-0" classCode="absolute top-0 right-4 hidden"/>
+                    </ul>
+                </div>
+            </Transition>
+            </div>
+            <div class="w-full lg:w-[937px] lg:pt-10 lg:pl-11 p-2">
+                <div class="h-auto lg:h-[344px]">
+                    <Picture source="/src/assets/uploads/banner/banner.jpg" className="object-contain" alt="/src/assets/uploads/banner/banner.jpg" navigate="/home" />
                 </div>
             </div>
         </Container>
