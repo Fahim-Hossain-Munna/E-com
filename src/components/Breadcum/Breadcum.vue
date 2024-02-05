e4a0d0d186141f42d41a5b0e987672b9fbe57b01
Fahim Hossain Munna <87969487+Fahim-Hossain-Munna@users.noreply.github.com>
2024-02-05 17:00:22 +0600

    design with contact page

--- src/components/Breadcum/Breadcum.vue
+++ src/components/Breadcum/Breadcum.vue
@@ -1,14 +1,24 @@
+<script setup>
+defineProps({
+    pagename:String,
+    navigation:String,
+})
+</script>
 <template>
      <nav>
         <ul class="flex justify-start items-center gap-3">
             <li class="font-inner text-sm transition-all duration-500  hover:font-bold">
-                <a href="#"><span class="mr-2"><i class="fa-solid fa-house"></i></span>Home</a>
+                <router-link to="/">
+                    <span class="mr-2"><i class="fa-solid fa-house"></i></span>Home
+                </router-link>
             </li>
             <li class="font-inner text-sm">
                 <i class="fa-solid fa-chevron-right"></i>
             </li>
             <li class="font-inner text-sm transition-all duration-500  hover:font-bold">
-                <a href="#">Contact</a>
+                <router-link :to=navigation>
+                    {{ pagename }}
+                </router-link>
             </li>
         </ul>
     </nav>
--- src/components/Contact/Contact.vue
+++ src/components/Contact/Contact.vue
@@ -1,5 +1,7 @@
 <script setup>
 import Container from '../layouts/Container.vue';
+import Button from '../layouts/Button.vue';
+import Breadcum from '../Breadcum/Breadcum.vue';
 
 
 </script>
@@ -7,7 +9,56 @@ import Container from '../layouts/Container.vue';
 <template>
     <div>
         <Container>
-           
+           <div class="my-20">
+            <Breadcum pagename="Contact" navigation="/contact" />
+           </div>
+           <div class="flex justify-between mb-[140px]">
+            <div class="w-[340px] h-[457px] py-10 px-9">
+                <div class="flex flex-col">
+                    <div>
+                        <div class="flex items-center gap-4 mb-6">
+                        <div class="flex justify-center items-center w-[50px] h-[50px] rounded-full bg-[#DB4444] text-white">
+                            <i class="fa-solid fa-phone"></i>
+                        </div>
+                        <h2 class="font-pop font-medium text-base">Call To Us</h2>
+                    </div>
+                    <div class="border-b pb-8 flex flex-col gap-4">
+                        <h2 class="font-pop text-sm">We are available 24/7, 7 days a week.</h2>
+                        <p class="font-pop text-sm">Phone: +8801611112222</p>
+                    </div>
+                    </div>
+                    <!-- .... -->
+                    <div class="mt-8">
+                        <div class="flex items-center gap-4 mb-6">
+                        <div class="flex justify-center items-center w-[50px] h-[50px] rounded-full bg-[#DB4444] text-white">
+                            <i class="fa-regular fa-envelope"></i>
+                        </div>
+                        <h2 class="font-pop font-medium text-base">Write To US</h2>
+                    </div>
+                    <div class="flex flex-col gap-4">
+                        <p class="font-pop text-sm">Fill out our form and we will contact you within 24 hours.</p>
+                        <p class="font-pop text-sm">We are available 24/7, 7 days a week.</p>
+                        <p class="font-pop text-sm">Phone: +8801611112222</p>
+                    </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- contact from part -->
+            <div class="w-[800px] p-8">
+                <div class="flex justify-between mb-8">
+                    <input type="text" placeholder="Your Name *" class="rounded w-[235px] py-[13px] px-4 bg-[#f5f5f5] placeholder:font-pop placeholder:text-base">
+                    <input type="text" placeholder="Your Email *" class="rounded w-[235px] py-[13px] px-4 bg-[#f5f5f5] placeholder:font-pop placeholder:text-base">
+                    <input type="text" placeholder="Your Phone *" class="rounded w-[235px] py-[13px] px-4 bg-[#f5f5f5] placeholder:font-pop placeholder:text-base">
+                </div>
+                <div class="mb-8">
+                    <textarea class="rounded w-[737px] bg-[#f5f5f5] py-[13px] px-4 placeholder:font-pop placeholder:text-base" rows="8" placeholder="Your Message *"></textarea>
+                </div>
+                <div class="flex justify-end">
+                    <Button title="Send Massage" />
+                </div>
+            </div>
+           </div>
         </Container>
     </div>
 </template>
